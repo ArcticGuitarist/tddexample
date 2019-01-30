000fd5b78f4efed3f7a78711280ffec2f8a64fe8
Chris Tisdale <cjtisdale21@gmail.com>
2019-01-29 18:49:43 -0600

    Making add car return created

---
+++ src/main/java/com/tdd/example/car/CarController.java
@@ -0,0 +1,21 @@
+package com.tdd.example.car;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController
+public class CarController {
+
+    private Set<Car> cars = new HashSet<>();
+
+    @RequestMapping(value = "/car", method = RequestMethod.PUT)
+    public @ResponseBody ResponseEntity<?> putCar(@RequestBody(required = true) Car car) {
+        // Add to DB not set this is just an example
+        cars.add(car);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
